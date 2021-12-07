2d35f480ba5bc79c92c4319dd62ea4f3b2f8bdd8
Franky1 <arvid.teichtmann@gmx.de>
2021-12-07 22:10:36 +0100

    tests with github actions

--- versioncheck.py
+++ versioncheck.py
@@ -6,3 +6,6 @@ import os
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 
 print(hashlib.sha256(GITHUB_TOKEN.encode()).hexdigest())
+
+os.environ["UPDATE_MAIN"] = True
+os.environ["UPDATE_RELEASE"] = True
