851bad4216abcdb66aab5af11ff0b8a06c1fc06f
Franky1 <Franky1@users.noreply.github.com>
2021-12-09 13:59:29 +0100

    Update versioncheck.py

--- versioncheck.py
+++ versioncheck.py
@@ -1,12 +1,1 @@
-# only test file
-
-import hashlib
-import os
-from pathlib import Path
-
-GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
-
-print(hashlib.sha256(GITHUB_TOKEN.encode()).hexdigest())
-
-Path("UPDATE_MAIN").touch()
-Path("UPDATE_RELEASE").touch()
+# TODO: update python script for production
